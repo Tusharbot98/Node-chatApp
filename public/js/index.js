a07795af27a3028f5acb11f6c3719209fc5e97f6
Tushar <tushar.nadkar98@gmail.com>
2018-10-19 10:36:22 +0530

    geo loatioon added

--- public/js/index.js
+++ public/js/index.js
@@ -27,5 +27,30 @@ jQuery('#message-form').on('submit',function(e){
   },function(){
 
   })
-
 })
+
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function(){
+ if (!navigator.geolocation) {
+   return alert('geo location is not supported by your browser')
+ }
+ navigator.geolocation.getCurrentPosition(function (position) {
+  socket.emit('createLocationMessage',{
+    longitude:position.coords.longitude,
+    latitude:position.coords.latitude
+  });
+ }, function () {
+   alert('unable to fetch location')
+ });})
+
+ socket.on('newLocationMessage',function(message){
+  var li = jQuery('<li></li>');
+  var  a =jQuery('<a target="_blank">My current location </a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href',message.url);
+  li.append(a);
+  jQuery('#messages').append(li);
+ })
